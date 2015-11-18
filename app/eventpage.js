4506d4789f313ecdbd94908268dd94ddbe8644c0
Volodymyr Buell <vbuell@gmail.com>
2015-11-18 16:45:59 -0500

    Alt-N style opening. Cyclic switching of opened tabs

--- app/eventpage.js
+++ app/eventpage.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log('hello');
-
 chrome.commands.onCommand.addListener(function(command) {
   if (command == 'launch_options') {
     chrome.tabs.create({
@@ -10,55 +8,59 @@ chrome.commands.onCommand.addListener(function(command) {
     return;
   }
 
-  commandToShortcut(command, function(shortcut) {
-    console.log('Command:', command, shortcut);
-    if (shortcut) {
-      shortcutToBookmarkNodes(shortcut, createTabsForBookmarkNodes);
+  console.log('Command:', command);
+  var tabIndex = commandToNumber(command);
+
+  shortcutToBookmarkNodes(tabIndex);
+});
+
+function commandToNumber(commandName) {
+  return parseInt(commandName.slice(-1)) - 1; // Because item at 0 position is an Apps button, let's do index non-zero based
+}
+
+function shortcutToBookmarkNodes(tabIndex) {
+  chrome.bookmarks.getChildren('1', function(array) {
+    var bookmark_item = array[tabIndex];
+    var url = bookmark_item.url;
+    openExistingTab(bookmark_item);
+  });
+}
+
+function openExistingTab(node) {
+    if (node.url) {
+        chrome.windows.getCurrent(function(win) {
+          chrome.tabs.getAllInWindow(win.id, function(tabs) {
+            chrome.tabs.getSelected(function(selected_tab) {
+              var start_idx = selected_tab.index;
+              for (var i = 0; i < tabs.length; i++) {
+                var tab_idx = (i + start_idx + 1);	// - tabs.length * ((i + start_idx + 1) % tabs.length) >> 0;
+                var normalized_idx = tab_idx >= tabs.length ? tab_idx - tabs.length : tab_idx
+                if (tabs[normalized_idx].url.indexOf(node.url) != -1) {
+                    chrome.tabs.update(tabs[normalized_idx].id, { active: true });
+                    return;
+                }
+              }
+              createTabsForBookmarkNode(node);
+            });
+          });
+       });
     }
-  });
-});
+}
 
-chrome.commands.getAll(function(commands) {
-  commands.forEach(function(command) {
-    console.log(command.name, command.shortcut);
-  });
-});
+function createTabsForBookmarkNode(node) {
+    if (node.url)
+      chrome.tabs.create({ url: node.url });
+//    else if (node.children)	// not sure that we should support that
+//      createTabsForBookmarkNodes(node.children);
+}
 
+
+// This section is called by View
 function getCommands(callback) {
   chrome.commands.getAll(function(commands) {
     callback(commands.filter(function(cmd) {
       return (cmd.name != 'launch_options');
     }));
-  });
-}
-
-function commandToShortcut(commandName, callback) {
-  getCommands(function(commands) {
-    var notFound = commands.every(function(command) {
-      if (command.name === commandName) {
-        callback(command.shortcut);
-        return false;
-      } else {
-        return true;
-      }
-    });
-
-    if (notFound)
-      callback();
-  });
-}
-
-function shortcutToBookmarkNodes(shortcut, callback) {
-  var bracketName = '[' + shortcut + ']';
-  chrome.bookmarks.search(bracketName, callback);
-}
-
-function createTabsForBookmarkNodes(nodes) {
-  nodes.forEach(function(node) {
-    if (node.url)
-      chrome.tabs.create({ url: node.url });
-    else if (node.children)
-      createTabsForBookmarkNodes(node.children);
   });
 }
 
